397ac76262faace27a8e29aa1468c65f5b75d280
Finci35 <2349fatih@gmail.com>
2022-12-15 23:32:57 +0100

    15 Ara

--- src/Uebung/Kapitalverdoppelung2.java
+++ src/Uebung/Kapitalverdoppelung2.java
@@ -9,23 +9,23 @@ public class Kapitalverdoppelung2 {
         double zinssatz;
         int jahre = 0;
 
-    // Eingabefenster
+        // Eingabefenster
         eingabe = JOptionPane.showInputDialog("Anfangskapital eingeben");
         startkapital = Double.parseDouble(eingabe);
         eingabe = JOptionPane.showInputDialog("Zinssatz eingeben");
         zinssatz = Double.parseDouble(eingabe);
 
-        //Verarbeitungsbreich
+        // Verarbeitungsbreich
         endkapital = startkapital;
-        while (endkapital < 2* startkapital) {
+        while (endkapital < 2 * startkapital) {
             jahre++;
-            endkapital = endkapital * (1 + zinssatz/100);
+            endkapital = endkapital * (1 + zinssatz / 100);
         }
-        //Ausgabe
+        // Ausgabe
         // %,.2f bedeutet die Darstellung mit fload und 2 Nachkommastellen.
         ausgabe = String.format("Das Startkapital von %,.2f €" + "\nhat sich in %d Jahren verdoppelt."
                 + "\nEndkapital: %,.2f €", startkapital, jahre, endkapital);
 
         JOptionPane.showMessageDialog(null, ausgabe);
-            }
+    }
 }
