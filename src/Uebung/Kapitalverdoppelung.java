050ff8ade7da1ed84145a1b5caae732dc40554ef
Finci35 <2349fatih@gmail.com>
2022-12-15 23:40:17 +0100

    11111

--- src/Uebung/Kapitalverdoppelung.java
+++ src/Uebung/Kapitalverdoppelung.java
@@ -1,11 +1,12 @@
 package Uebung;
+
 import javax.swing.JOptionPane;
 import java.text.DecimalFormat;
 
 public class Kapitalverdoppelung {
     public static void main(String[] args) {
-        //Startkapital Eingabe SK
-        //Zinssatz Eingabe ZS
+        // Startkapital Eingabe SK
+        // Zinssatz Eingabe ZS
         float SK, ZS;
         String StartKapital, Zinssatz;
         StartKapital = JOptionPane.showInputDialog("Geben Sie Startkapital ein:");
@@ -15,13 +16,13 @@ public class Kapitalverdoppelung {
         ZS = Integer.parseInt(Zinssatz);
 
         int jahr = 0;
-        //verzinstes Kapital => Kapital =SK
+        // verzinstes Kapital => Kapital =SK
         float VK = SK;
         DecimalFormat frmt = new DecimalFormat();
         frmt.setMaximumFractionDigits(2);
 
         do {
-            VK = VK * (1 + ZS / 100);
+            VK = VK * (1 + ZS / 200);
             jahr = jahr + 1;
         } while (VK < 2 * SK);
         System.out.println("Kapital: " + frmt.format(VK) + "€");
